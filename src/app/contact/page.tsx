1aef73ccf0f6c2815c20b4a90cb76a34bf3b97fb
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2026-01-12 02:24:12 +0600

    Setup done go to next mission..

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div className='my-20 gap-10 flex items-center justify-center '>
+            <h1>This is Contact Page..</h1>
+            <Button>Arafat rahman sani</Button>
+        </div>
+    );
+};
+
+export default ContactPage;
