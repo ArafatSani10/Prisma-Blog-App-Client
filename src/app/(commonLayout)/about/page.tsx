93aac0f91bc7915e22ffc4370048fd5640e2fe0b
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2026-01-17 21:17:18 +0600

    Authentication with Google OAuth succesfully done..

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -3,7 +3,7 @@ export default async function AboutPage() {
     await new Promise((resolve) => setTimeout(resolve, 4000))
     return (
         <div>
-            <h1>This is about page here...</h1>
+            <h1 className="text-2xl text-black dark:text-[#00baff]">This is about page here...</h1>
         </div>
     )
 }
